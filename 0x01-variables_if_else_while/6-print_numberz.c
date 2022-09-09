719a5e50f7ea4e5c632f6e30be3927df7154b627
Noellyn <Noellyn>
2022-09-09 14:54:37 -0700

    Prints single digit numbers using int and putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - prints all single digit numbers
+*starting with 0
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int s;
+
+for (s = 48; s < 58; s++)
+{
+putchar(s);
+}
+putchar('\n');
+return (0);
+}
